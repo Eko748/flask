757e0537cf835534e2b51ea2014a0fb42089663c
Eko Permana <permanaeko74@gmail.com>
2022-07-31 22:11:26 +0700

    Role, Login, Token authorized, & Pagination

--- app/controller/DosenController.py
+++ app/controller/DosenController.py
@@ -2,7 +2,8 @@ from app.model.dosen import Dosen
 from app.model.mahasiswa import Mahasiswa
 
 from app import response, app, db
-from flask import request
+from flask import request, jsonify, abort
+import math
 
 def index():
     try:
@@ -136,3 +137,68 @@ def hapus(id):
     except Exception as e:
         print(e)
         
+# Pagination
+def get_pagination(clss, url, start, limit):
+    # ambil data select
+    results = clss.query.all()
+    # ubah format
+    data = formatarray(results)
+    # hitung jumlah data
+    count = len(data)
+    
+    obj = {}
+    
+    if count < start:
+        obj['success'] = False
+        obj['message'] = "Page yang dipilih melewati batas total data!"
+        return obj
+    else:
+        obj['success'] = True
+        obj['start_page'] = start
+        obj['per_page'] = limit
+        obj['total_data'] = count
+        obj['total_page'] = math.ceil(count/limit)
+        
+        # previous link
+        if start == 1:
+            obj['previous'] = ''
+        else:
+            start_copy = max(1, start-limit)
+            limit_copy = start -1
+            obj['previous'] = url + '?start=%d&limit=%d' % (start_copy, limit_copy)
+            
+        # next link
+        if start +  limit > count:
+            obj['next'] = ''
+        else:
+            start_copy = start + limit
+            obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
+            
+        obj['results'] = data[(start - 1): (start - 1 + limit)]
+        return obj
+        
+# fungsi paging
+def paginate():
+    # ambil parameter get
+    # sample
+    
+    start = request.args.get('start')
+    limit = request.args.get('limit')
+    
+    try:
+        if start == None or limit == None:
+            return jsonify(get_pagination(
+                Dosen,
+                'http://127.0.0.1:5000/api/dosen/page',
+                start = request.args.get('start', 1),
+                limit = request.args.get('limit', 3)
+            ))
+        else:
+            return jsonify(get_pagination(
+                Dosen,
+                'http://127.0.0.1:5000/api/dosen/page',
+                start = int(start),
+                limit = int(limit)
+            ))
+    except Exception as e:
+        print(e)
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,41 @@
+from app import app, response
 from flask import request
-from app import app
+from flask import jsonify
 from app.controller import DosenController
+from app.controller import UserController
+from flask_jwt_extended import current_user, get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return response.success(current_user, 'Sukses')
+
 @app.route('/dosen', methods=['GET', 'POST'])
+@jwt_required()
 def dosens():
     if request.method == 'GET':
         return DosenController.index()
     else:
         return DosenController.save()
+    
+# buat rute paging
+@app.route('/api/dosen/page', methods=['GET'])
+def pagination():
+    return DosenController.paginate()
+    
+@app.route('/file-upload', methods=['POST'])
+def uploads():
+    return UserController.upload()
+    
+@app.route('/createadmin', methods=['POST'])
+def admins():
+    return UserController.buatAdmin()
 
 @app.route('/dosen/<id>', methods=['GET', 'PUT', 'DELETE'])
 def dosensDetail(id):
@@ -21,5 +45,9 @@ def dosensDetail(id):
         return DosenController.ubah(id)
     elif request.method == 'DELETE':
         return DosenController.hapus(id)
+    
+@app.route('/login', methods=['POST'])
+def logins():
+    return UserController.login()
 
     
